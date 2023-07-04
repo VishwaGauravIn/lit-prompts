86b50fa1ab7e8855912384088dfc9c4f509721c9
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-07-05 00:44:35 +0530

    added data for temp testing

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,20 +6,19 @@ import {
   BsLink45Deg,
 } from "react-icons/bs";
 
-export default function Card() {
+export default function Card({ act, prompt }) {
   const colorClass = `grad${Math.floor(Math.random() * 14) + 1}`;
   return (
     <div className="flex flex-col gap-1 justify-center items-center w-max relative z-10 m-6 self-center">
       <div className="w-80 bg-zinc-800/90 border-2 border-zinc-800 backdrop-blur-lg rounded-lg p-4 text-white text-justify">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        {act}
       </div>
       <span className="w-4 h-4 bg-zinc-800 rounded-full" />
       <span className="w-2 h-2 bg-zinc-800 rounded-full" />
       <div
         className={`relative w-80 ${colorClass} p-4 rounded-lg text-justify z-10 font-semibold`}
       >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
-        tempora, sint ex error perferendis reprehenderit.
+        {prompt}
       </div>
       <div className="relative w-[21rem] bg-zinc-800/90 backdrop-blur-lg h-16 -mt-8 rounded-lg p-4 pt-5 text-fuchsia-900 rotate-12"></div>
       <div
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import AnimatedBG from "@/components/AnimatedBG";
 import Card from "@/components/Card";
+import { prompts } from "@/data/prompts";
 import { Ysabeau } from "next/font/google";
 
 const customFont = Ysabeau({ subsets: ["latin"] });
@@ -7,21 +8,12 @@ const customFont = Ysabeau({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <main className={`${customFont.className} relative scroll-smooth flex flex-wrap gap-10 justify-center`}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <main
+        className={`${customFont.className} relative scroll-smooth flex flex-wrap gap-10 justify-center`}
+      >
+        {prompts.map((prompt) => (
+          <Card key={prompt.id} act={prompt.act} prompt={prompt.prompt} />
+        ))}
       </main>
       <AnimatedBG />
     </>
