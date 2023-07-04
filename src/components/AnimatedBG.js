816267d5901d58752c785eca321bd7ab0739cb1e
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-07-04 18:37:27 +0530

    added animated bg + basic card design + cleanup

---
+++ src/components/AnimatedBG.js
@@ -0,0 +1,124 @@
+import React from "react";
+
+export default function AnimatedBG() {
+  return (
+    <div className="fixed left-0 top-0 right-0 bottom-0 -z-10 -translate-x-1/2">
+      <div className="max-w-max -rotate-45">
+        <p className="marquee">
+          BERT • NEZHA • LaMDA • T5 • RoBERTa • ALBERT • GPT-Neo • Meena •
+          Turing NLG • Chinchilla • Gopher BERT • NEZHA • LaMDA • T5 • RoBERTa •
+          ALBERT • GPT-Neo • Meena • Turing NLG • Chinchilla • Gopher
+        </p>
+        <p className="marquee">
+          XLNet • Megatron-Turing NLG • ChatGPT • DialoGPT • GPT-4 • LaMDA 2 •
+          PaLM 2 • Chinchilla 2 • GPT-J • Cleverbot • Replika XLNet •
+          Megatron-Turing NLG • ChatGPT • DialoGPT • GPT-4 • LaMDA 2 • PaLM 2 •
+          Chinchilla 2 • GPT-J • Cleverbot • Replika
+        </p>
+        <p className="marquee">
+          Meena • GPT-NeoX • Bard • Imitator • Xiaoice • GPT-3 • PaLM •
+          Cleverbot • NEZHA 2 • LaMDA • RoBERTa Meena • GPT-NeoX • Bard •
+          Imitator • Xiaoice • GPT-3 • PaLM • Cleverbot • NEZHA 2 • LaMDA •
+          RoBERTa
+        </p>
+        <p className="marquee">
+          ALBERT • Gopher • Turing NLG • XLNet • GPT-J • ChatGPT • Xiaoice •
+          Replika • Meena • GPT-NeoX • Bard ALBERT • Gopher • Turing NLG • XLNet
+          • GPT-J • ChatGPT • Xiaoice • Replika • Meena • GPT-NeoX • Bard
+        </p>
+        <p className="marquee">
+          PaLM 2 • GPT-4 • Chinchilla 2 • LaMDA 2 • Chinchilla • NEZHA • XLNet •
+          Megatron-Turing NLG • RoBERTa • ALBERT • T5 PaLM 2 • GPT-4 •
+          Chinchilla 2 • LaMDA 2 • Chinchilla • NEZHA • XLNet • Megatron-Turing
+          NLG • RoBERTa • ALBERT • T5
+        </p>
+        <p className="marquee">
+          Meena • GPT-Neo • Replika • Imitator • Xiaoice • GPT-3 • Cleverbot •
+          Bard • Turing NLG • XLNet • Gopher Meena • GPT-Neo • Replika •
+          Imitator • Xiaoice • GPT-3 • Cleverbot • Bard • Turing NLG • XLNet •
+          Gopher
+        </p>
+        <p className="marquee">
+          Chinchilla • NEZHA • Meena • Gopher • GPT-J • ChatGPT • DialoGPT •
+          GPT-Neo • PaLM 2 • Chinchilla 2 • GPT-4 Chinchilla • NEZHA • Meena •
+          Gopher • GPT-J • ChatGPT • DialoGPT • GPT-Neo • PaLM 2 • Chinchilla 2
+          • GPT-4
+        </p>
+        <p className="marquee">
+          LaMDA • PaLM • T5 • RoBERTa • ALBERT • GPT-NeoX • Bard • Imitator •
+          Xiaoice • GPT-4 • LaMDA 2 LaMDA • PaLM • T5 • RoBERTa • ALBERT •
+          GPT-NeoX • Bard • Imitator • Xiaoice • GPT-4 • LaMDA 2
+        </p>
+        <p className="marquee">
+          BERT • RoBERTa • ALBERT • T5 • Megatron-Turing NLG • ChatGPT •
+          DialoGPT • Xiaoice • Replika • Imitator • GPT-4 BERT • RoBERTa •
+          ALBERT • T5 • Megatron-Turing NLG • ChatGPT • DialoGPT • Xiaoice •
+          Replika • Imitator • GPT-4
+        </p>
+        <p className="marquee">
+          Cleverbot • NEZHA 2 • LaMDA 2 • PaLM 2 • Chinchilla 2 • GPT-J •
+          GPT-NeoX • Bard • GPT-3 • Meena • Gopher Cleverbot • NEZHA 2 • LaMDA 2
+          • PaLM 2 • Chinchilla 2 • GPT-J • GPT-NeoX • Bard • GPT-3 • Meena •
+          Gopher
+        </p>
+        <p className="marquee">
+          BERT • NEZHA • LaMDA • T5 • RoBERTa • ALBERT • GPT-Neo • Meena •
+          Turing NLG • Chinchilla • Gopher BERT • NEZHA • LaMDA • T5 • RoBERTa •
+          ALBERT • GPT-Neo • Meena • Turing NLG • Chinchilla • Gopher
+        </p>
+        <p className="marquee">
+          XLNet • Megatron-Turing NLG • ChatGPT • DialoGPT • GPT-4 • LaMDA 2 •
+          PaLM 2 • Chinchilla 2 • GPT-J • Cleverbot • Replika XLNet •
+          Megatron-Turing NLG • ChatGPT • DialoGPT • GPT-4 • LaMDA 2 • PaLM 2 •
+          Chinchilla 2 • GPT-J • Cleverbot • Replika
+        </p>
+        <p className="marquee">
+          Meena • GPT-NeoX • Bard • Imitator • Xiaoice • GPT-3 • PaLM •
+          Cleverbot • NEZHA 2 • LaMDA • RoBERTa Meena • GPT-NeoX • Bard •
+          Imitator • Xiaoice • GPT-3 • PaLM • Cleverbot • NEZHA 2 • LaMDA •
+          RoBERTa
+        </p>
+        <p className="marquee">
+          ALBERT • Gopher • Turing NLG • XLNet • GPT-J • ChatGPT • Xiaoice •
+          Replika • Meena • GPT-NeoX • Bard ALBERT • Gopher • Turing NLG • XLNet
+          • GPT-J • ChatGPT • Xiaoice • Replika • Meena • GPT-NeoX • Bard
+        </p>
+        <p className="marquee">
+          PaLM 2 • GPT-4 • Chinchilla 2 • LaMDA 2 • Chinchilla • NEZHA • XLNet •
+          Megatron-Turing NLG • RoBERTa • ALBERT • T5 PaLM 2 • GPT-4 •
+          Chinchilla 2 • LaMDA 2 • Chinchilla • NEZHA • XLNet • Megatron-Turing
+          NLG • RoBERTa • ALBERT • T5
+        </p>
+        <p className="marquee">
+          Meena • GPT-Neo • Replika • Imitator • Xiaoice • GPT-3 • Cleverbot •
+          Bard • Turing NLG • XLNet • Gopher Meena • GPT-Neo • Replika •
+          Imitator • Xiaoice • GPT-3 • Cleverbot • Bard • Turing NLG • XLNet •
+          Gopher
+        </p>
+        <p className="marquee">
+          Chinchilla • NEZHA • Meena • Gopher • GPT-J • ChatGPT • DialoGPT •
+          GPT-Neo • PaLM 2 • Chinchilla 2 • GPT-4 Chinchilla • NEZHA • Meena •
+          Gopher • GPT-J • ChatGPT • DialoGPT • GPT-Neo • PaLM 2 • Chinchilla 2
+          • GPT-4
+        </p>
+        <p className="marquee">
+          LaMDA • PaLM • T5 • RoBERTa • ALBERT • GPT-NeoX • Bard • Imitator •
+          Xiaoice • GPT-4 • LaMDA 2 LaMDA • PaLM • T5 • RoBERTa • ALBERT •
+          GPT-NeoX • Bard • Imitator • Xiaoice • GPT-4 • LaMDA 2
+        </p>
+        <p className="marquee">
+          BERT • RoBERTa • ALBERT • T5 • Megatron-Turing NLG • ChatGPT •
+          DialoGPT • Xiaoice • Replika • Imitator • GPT-4 BERT • RoBERTa •
+          ALBERT • T5 • Megatron-Turing NLG • ChatGPT • DialoGPT • Xiaoice •
+          Replika • Imitator • GPT-4
+        </p>
+        <p className="marquee">
+          Cleverbot • NEZHA 2 • LaMDA 2 • PaLM 2 • Chinchilla 2 • GPT-J •
+          GPT-NeoX • Bard • GPT-3 • Meena • Gopher Cleverbot • NEZHA 2 • LaMDA 2
+          • PaLM 2 • Chinchilla 2 • GPT-J • GPT-NeoX • Bard • GPT-3 • Meena •
+          Gopher
+        </p>
+      </div>
+    </div>
+  );
+}
