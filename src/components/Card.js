ddec8780c3cd7ef1e2de9f13c451381353ec0ab0
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-07-05 00:17:22 +0530

    card design impv

---
+++ src/components/Card.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  BsWhatsapp,
+  BsClipboard,
+  BsBookmarkHeart,
+  BsLink45Deg,
+} from "react-icons/bs";
+
+export default function Card() {
+  return (
+    <div className="flex flex-col gap-1 justify-center items-center w-max m-2 relative z-10">
+      <div className="w-80 bg-zinc-800/90 border-2 border-zinc-800 backdrop-blur-lg rounded-lg p-4 text-white text-justify">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      </div>
+      <span className="w-4 h-4 bg-zinc-800 rounded-full" />
+      <span className="w-2 h-2 bg-zinc-800 rounded-full" />
+      <div
+        contentEditable
+        className="relative w-80 grad1 p-4 rounded-lg border border-b-0 text-justify z-10 font-semibold"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
+        tempora, sint ex error perferendis reprehenderit.
+      </div>
+      <div className="relative w-[21rem] bg-zinc-800/90 border-2 border-zinc-800 backdrop-blur-lg h-16 -mt-8 rounded-lg p-4 pt-5 text-fuchsia-900 rotate-12"></div>
+      <div className="relative w-[21rem] grad1 py-4 px-8 h-16 rounded-lg -mt-4 border border-t-0 text-justify z-10 font-semibold -rotate-6 flex gap-2 justify-between">
+        <BsClipboard title="Copy to Clipboard" className="w-6 h-6 opacity-90 hover:opacity-100 ease-in-out transition-all active:scale-90 cursor-pointer duration-100 stroke-[0.2]" />
+        <BsLink45Deg title="Copy Link" className="w-7 h-7 opacity-90 hover:opacity-100 ease-in-out transition-all active:scale-90 cursor-pointer duration-100 stroke-[0.2]" />
+        <BsWhatsapp title="Share on WhatsApp" className="w-6 h-6 opacity-90 hover:opacity-100 ease-in-out transition-all active:scale-90 cursor-pointer duration-100 stroke-[0.1]" />
+        <BsBookmarkHeart title="Bookmark it" className="w-6 h-6 opacity-90 hover:opacity-100 ease-in-out transition-all active:scale-90 cursor-pointer duration-100 stroke-[0.2]" />
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import AnimatedBG from "@/components/AnimatedBG";
+import Card from "@/components/Card";
 import { Ysabeau } from "next/font/google";
 
 const customFont = Ysabeau({ subsets: ["latin"] });
@@ -6,21 +7,10 @@ const customFont = Ysabeau({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <main className={`${customFont.className} relative`}>
-        <div className="flex flex-col gap-1 justify-center items-center w-max m-2 shadow-lg relative z-10">
-          <div className="w-80 bg-zinc-800 rounded-lg p-4 text-white text-justify">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          </div>
-          <span className="w-4 h-4 bg-zinc-800 rounded-full" />
-          <span className="w-2 h-2 bg-zinc-800 rounded-full" />
-          <div className="relative w-80 grad1 p-4 rounded-lg border border-b-0 text-justify z-10 font-semibold">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
-            tempora, sint ex error perferendis reprehenderit.
-          </div>
-          <div className="relative w-80 bg-fuchsia-200 h-12 -mt-4 border rounded-lg p-4 pt-5 text-fuchsia-900"></div>
-        </div>
+      <main className={`${customFont.className} relative scroll-smooth`}>
+        <Card />
       </main>
-      <AnimatedBG/>
+      <AnimatedBG />
     </>
   );
 }
