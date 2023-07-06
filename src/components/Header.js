f2fda54c48ed0b187edb7591bf18fb374008e30d
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-07-06 05:51:13 +0530

    basic header design

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+
+export default function Header() {
+  useEffect(() => {
+    try {
+      const scrollEvent = window.addEventListener("scroll", () => {
+        let scrollYval = scrollY;
+        if (scrollYval > 50) {
+          document.getElementById("navbar").style.backgroundColor =
+            "rgb(18, 18, 18, 0.9)";
+        } else {
+          document.getElementById("navbar").style.backgroundColor =
+            "transparent";
+        }
+      });
+    } catch {
+      window.removeEventListener(scrollEvent);
+    }
+  }, []);
+  return (
+    <div
+      id="navbar"
+      className="flex justify-between w-full px-6 py-3 top-0 sticky z-30"
+    >
+      <div className="flex justify-center items-center text-2xl font-bold text-yellow-400">
+        <img src="/logo.png" alt="" className="w-14 h-14" />
+        Lit Prompts
+      </div>
+    </div>
+  );
+}
--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -1,6 +1,8 @@
 import Card2 from "@/components/Card2";
+import Header from "@/components/Header";
 import { prompts } from "@/data/prompts";
 import { Open_Sans } from "next/font/google";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
@@ -13,9 +15,13 @@ export default function Home({ data, pageNumber, maxPageNumber }) {
   }
   return (
     <>
+      <Head>
+        <title>Lit Prompts : Best AI prompts 🔥</title>
+      </Head>
       <main
         className={`${customFont.className} relative scroll-smooth flex flex-col ring flex-wrap gap-10 items-center`}
       >
+        <Header />
         <ResponsiveMasonry
           columnsCountBreakPoints={{ 350: 1, 800: 2, 1230: 3 }}
           className="w-full max-w-7xl"
@@ -31,11 +37,13 @@ export default function Home({ data, pageNumber, maxPageNumber }) {
             ))}
           </Masonry>
         </ResponsiveMasonry>
+        <div className="h-16"></div>
       </main>
     </>
   );
 }
 
+// function to paginate the array
 const paginateArray = (array, pageNumber, pageSize) => {
   const page = array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
   return page;
@@ -43,13 +51,15 @@ const paginateArray = (array, pageNumber, pageSize) => {
 
 export const getServerSideProps = async (pageContext) => {
   const maxPageNumber = Math.ceil(prompts.length / 10);
+  // default props : redirecting to page 1 if page number is invalid
   const defaultProps = {
-    props: {
-      data: paginateArray(prompts, 1, 10),
-      pageNumber: 1,
-      maxPageNumber: maxPageNumber,
+    redirect: {
+      permanent: false,
+      destination: "/1",
     },
+    props: {},
   };
+  // using try-catch for error handling
   try {
     const pageNumber = pageContext.query.slug;
     if (pageNumber > 0 && pageNumber <= maxPageNumber) {
