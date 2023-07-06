87057d4add089590d41992ead936808684bee614
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-07-07 03:49:16 +0530

    added tooltip

--- src/components/Card2.js
+++ src/components/Card2.js
@@ -5,6 +5,7 @@ import {
   BsBookmarkHeart,
   BsLink45Deg,
 } from "react-icons/bs";
+import { Tooltip } from "react-tooltip";
 
 export default function Card2({ index, act, prompt, fullSize }) {
   function copyLink() {
@@ -29,22 +30,42 @@ export default function Card2({ index, act, prompt, fullSize }) {
       <hr className="opacity-10" />
       {/* Tools */}
       <div className="flex gap-8 text-zinc-400 justify-around -mt-1 -mb-3">
-        <div className="group hover:bg-yellow-100/20 p-2 rounded-full w-12 h-12 flex justify-center items-center">
-          <BsClipboard className="w-6 h-6 transition-all ease-in-out group-hover:text-yellow-300 active:scale-90 cursor-pointer" />
+        <div
+          className="group hover:bg-yellow-100/20 p-2 rounded-full w-12 h-12 flex justify-center items-center cursor-pointer"
+          onClick={() => navigator.clipboard.writeText(prompt)}
+          data-tooltip-id="tooltip"
+          data-tooltip-content="Copy Prompt"
+          data-tooltip-delay-show={1000}
+        >
+          <BsClipboard className="w-6 h-6 transition-all ease-in-out group-hover:text-yellow-300 active:scale-90 outline-none" />
         </div>
-        <div className="group hover:bg-cyan-100/20 p-2 rounded-full w-12 h-12 flex justify-center items-center">
-          <BsLink45Deg
-            onClick={() => copyLink()}
-            className="w-7 h-7 transition-all ease-in-out group-hover:text-cyan-300 active:scale-90 cursor-pointer"
-          />
+        <div
+          className="group hover:bg-cyan-100/20 p-2 rounded-full w-12 h-12 flex justify-center items-center cursor-pointer"
+          onClick={() => copyLink()}
+          data-tooltip-id="tooltip"
+          data-tooltip-content="Copy Link"
+          data-tooltip-delay-show={1000}
+        >
+          <BsLink45Deg className="w-7 h-7 transition-all ease-in-out group-hover:text-cyan-300 active:scale-90 outline-none" />
         </div>
-        <div className="group hover:bg-green-100/20 p-2 rounded-full w-12 h-12 flex justify-center items-center">
-          <BsWhatsapp className="w-6 h-6 transition-all ease-in-out group-hover:text-green-300 active:scale-90 cursor-pointer" />
+        <div
+          className="group hover:bg-green-100/20 p-2 rounded-full w-12 h-12 flex justify-center items-center cursor-pointer"
+          data-tooltip-id="tooltip"
+          data-tooltip-content="Share on WhatsApp"
+          data-tooltip-delay-show={1000}
+        >
+          <BsWhatsapp className="w-6 h-6 transition-all ease-in-out group-hover:text-green-300 active:scale-90 outline-none" />
         </div>
-        <div className="group hover:bg-red-100/20 p-2 rounded-full w-12 h-12 flex justify-center items-center">
-          <BsBookmarkHeart className="w-6 h-6 transition-all ease-in-out group-hover:text-red-300 active:scale-90 cursor-pointer" />
+        <div
+          className="group hover:bg-red-100/20 p-2 rounded-full w-12 h-12 flex justify-center items-center cursor-pointer"
+          data-tooltip-id="tooltip"
+          data-tooltip-content="Save this prompt"
+          data-tooltip-delay-show={1000}
+        >
+          <BsBookmarkHeart className="w-6 h-6 transition-all ease-in-out group-hover:text-red-300 active:scale-90 outline-none" />
         </div>
       </div>
+      <Tooltip id="tooltip" />
     </div>
   );
 }
