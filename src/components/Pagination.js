a641fabdbddd09698d4b25892566ff8794d0e387
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2023-07-07 16:27:01 +0530

    pagination with animation

---
+++ src/components/Pagination.js
@@ -0,0 +1,42 @@
+import { useRouter } from "next/router";
+import React from "react";
+import { BsCaretLeftFill, BsCaretRightFill } from "react-icons/bs";
+
+export default function Pagination({ pageNumber, maxPageNumber }) {
+  const router = useRouter();
+  return (
+    <div className="flex justify-center gap-8 mb-4 items-center">
+      {/* Previous Button */}
+      <button
+        className="flex items-center bg-zinc-800 relative rounded-full text-white pl-4 pr-6 py-2 disabled:opacity-50 disabled:cursor-not-allowed active:scale-95 transition-all ease-in-out duration-100 shadow-xl"
+        disabled={pageNumber === 1}
+        onClick={() => {
+          // double checking to avoid inspect element hackers, lol
+          if (pageNumber > 1) {
+            router.push(`/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
+          }
+        }}
+      >
+        <BsCaretLeftFill /> Prev<span className="hidden sm:block">ious
+        Page</span>
+      </button>
+      <span className="">
+        <span className="text-2xl font-bold mr-1">#</span>
+        {pageNumber}/{maxPageNumber}
+      </span>
+      {/* Next Button */}
+      <button
+        className="flex items-center bg-zinc-800 relative rounded-full text-white pl-6 pr-4 py-2 disabled:opacity-50 disabled:cursor-not-allowed active:scale-95 transition-all ease-in-out duration-100 shadow-xl"
+        disabled={pageNumber === maxPageNumber}
+        onClick={() => {
+          if (pageNumber < maxPageNumber) {
+            router.push(`/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
+          }
+        }}
+      >
+        Next<span className="hidden sm:block">&nbsp;Page</span>
+        <BsCaretRightFill />
+      </button>
+    </div>
+  );
+}
--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -1,9 +1,11 @@
 import Card2 from "@/components/Card2";
 import Header from "@/components/Header";
+import Pagination from "@/components/Pagination";
 import { prompts } from "@/data/prompts";
 import { Open_Sans } from "next/font/google";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 const customFont = Open_Sans({ subsets: ["latin"] });
@@ -13,31 +15,45 @@ export default function Home({ data, pageNumber, maxPageNumber }) {
   if (isReady && router) {
     router.push(`/${pageNumber}`);
   }
+  // Applying fade-appear animation on page change for smooth transition [not a good practice, but works for now]
+  useEffect(() => {
+    if (isReady) {
+      document.getElementById("masonry").classList.add("fade-appear");
+      setTimeout(() => {
+        document.getElementById("masonry").classList.remove("fade-appear");
+      }, 200);
+    }
+  }, [pageNumber]);
   return (
     <>
       <Head>
         <title>Lit Prompts : Best AI prompts 🔥</title>
       </Head>
       <main
-        className={`${customFont.className} relative scroll-smooth flex flex-col flex-wrap gap-10 items-center`}
+        className={`${customFont.className} relative scroll-smooth flex flex-col flex-wrap gap-2 sm:gap-10 items-center`}
       >
         <Header searchBarvisible={true} />
-        <ResponsiveMasonry
-          columnsCountBreakPoints={{ 350: 1, 800: 2, 1230: 3 }}
-          className="w-full max-w-7xl"
-        >
-          <Masonry>
-            {data.map((prompt, i) => (
-              <Card2
-                key={i}
-                index={prompt.index}
-                act={prompt.act}
-                prompt={prompt.prompt}
-              />
-            ))}
-          </Masonry>
-        </ResponsiveMasonry>
-        <div className="h-16"></div>
+        <div id="masonry" className="w-full flex justify-center">
+          <ResponsiveMasonry
+            columnsCountBreakPoints={{ 350: 1, 800: 2, 1230: 3 }}
+            className="w-full max-w-7xl"
+          >
+            <Masonry>
+              {data.map((prompt, i) => (
+                <Card2
+                  key={i}
+                  index={prompt.index}
+                  act={prompt.act}
+                  prompt={prompt.prompt}
+                />
+              ))}
+            </Masonry>
+          </ResponsiveMasonry>
+        </div>
+        <Pagination
+          pageNumber={parseInt(pageNumber)}
+          maxPageNumber={parseInt(maxPageNumber)}
+        />
       </main>
     </>
   );
